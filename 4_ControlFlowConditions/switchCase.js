3d90cf07b31891a32e480585a15a8b07441a3545
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-01 15:35:05 +0530

    Add switchCase.js
    This file contains the data about the switch cases used in the javaScript

---
+++ 4_ControlFlowConditions/switchCase.js
@@ -0,0 +1,36 @@
+// syntax for switch case
+/*
+switch (key) {
+    case value1:
+        // code to execute if expression equals value1
+        break;
+        case value2:
+            // code to execute if expression equals value2
+            break;
+            // add more cases as needed
+            default: // optional
+            // code to execute if expression does not match any of the above cases
+            break;
+            }
+            */
+
+    const month = 3
+    switch (month) {
+        case 1:
+            // console.log("Januaray");
+            break;
+        case 2:
+            // console.log("Feb");
+            break;
+        case 3:
+            // console.log("March");
+            break; 
+        case 4: 
+            // console.log("April");
+            break;
+    
+        default:
+            console.log("Invalid month");
+            
+            break;
+    }
